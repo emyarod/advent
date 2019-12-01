e76a8a6730e6582716a46d0d85fb2c143bf7d987
emyarod <afw5059@gmail.com>
2019-12-01 14:53:47 -0800

    add 2019/01 part 2

--- 2019/01/index.js
+++ 2019/01/index.js
@@ -18,10 +18,23 @@ const getModuleMasses = async () => {
     return null;
   }
 };
+const calculateFuel = input => {
+  const fuel = Math.floor(input / 3) - 2;
+  return fuel >= 0 ? fuel : 0;
+};
 
 (async () => {
   const masses = await getModuleMasses();
+  console.log(masses.reduce((acc, curr) => acc + calculateFuel(curr), 0)); // 3442987
   console.log(
-    masses.reduce((acc, curr) => acc + (Math.floor(curr / 3) - 2), 0)
+    masses.reduce((acc, curr) => {
+      let totalFuel = 0;
+      let fuel = calculateFuel(curr);
+      while (fuel) {
+        totalFuel += fuel;
+        fuel = calculateFuel(fuel);
+      }
+      return acc + totalFuel;
+    }, 0) // 5161601
   );
 })();
