80f2b3809014bf9a89e219b762ab52b3719ca237
emyarod <afw5059@gmail.com>
2019-12-03 12:52:59 -0800

    add 2019/02 part 1

---
+++ 2019/02/index.js
@@ -0,0 +1,54 @@
+import path from 'path';
+import { promisify } from 'util';
+import { readFile } from 'fs';
+
+const readFilePromise = promisify(readFile);
+const getInput = async () => {
+  try {
+    const data = await readFilePromise(
+      path.resolve(__dirname, 'input.txt'),
+      'utf8'
+    );
+    return data
+      .trim()
+      .split(',')
+      .map(Number);
+  } catch (err) {
+    console.log('ERROR:', err);
+    return null;
+  }
+};
+const runIntcode = input => {
+  for (
+    let i = 0, j = i + 1, k = i + 2, l = i + 3;
+    i < input.length;
+    i += 4, j += 4, k += 4, l += 4
+  ) {
+    const opcode = input[i];
+    const noun = input[j];
+    const verb = input[k];
+    const resultIdx = input[l];
+    if (opcode === 99) {
+      break;
+    }
+    if (opcode === 1) {
+      input[resultIdx] = input[noun] + input[verb];
+    }
+    if (opcode === 2) {
+      input[resultIdx] = input[noun] * input[verb];
+    }
+  }
+  return input;
+};
+
+console.log(runIntcode([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50])); // 3500,9,10,70,2,3,11,0,99,30,40,50
+console.log(runIntcode([1, 0, 0, 0, 99])); // 2,0,0,0,99
+console.log(runIntcode([2, 3, 0, 3, 99])); // 2,3,0,6,99
+console.log(runIntcode([2, 4, 4, 5, 99, 0])); // 2,4,4,5,99,9801
+
+(async () => {
+  const input = await getInput();
+  input[1] = 12;
+  input[2] = 2;
+  console.log(runIntcode(input)[0]);
+})();
