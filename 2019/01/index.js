e9c43db9d21164ef3c842a69bc96e26534ea70e5
emyarod <afw5059@gmail.com>
2019-12-03 16:04:57 -0800

    recurse

--- 2019/01/index.js
+++ 2019/01/index.js
@@ -18,23 +18,25 @@ const getModuleMasses = async () => {
     return null;
   }
 };
-const calculateFuel = input => {
-  const fuel = Math.floor(input / 3) - 2;
-  return fuel >= 0 ? fuel : 0;
+const calculateFuel = input => Math.max(Math.floor(input / 3) - 2, 0);
+const calculateTotalFuel = input => {
+  const fuel = calculateFuel(input);
+  return (fuel && fuel + calculateTotalFuel(fuel)) || 0;
 };
 
 (async () => {
   const masses = await getModuleMasses();
   console.log(masses.reduce((acc, curr) => acc + calculateFuel(curr), 0)); // 3442987
-  console.log(
-    masses.reduce((acc, curr) => {
-      let totalFuel = 0;
-      let fuel = calculateFuel(curr);
-      while (fuel) {
-        totalFuel += fuel;
-        fuel = calculateFuel(fuel);
-      }
-      return acc + totalFuel;
-    }, 0) // 5161601
-  );
+  // console.log(
+  //   masses.reduce((acc, curr) => {
+  //     let totalFuel = 0;
+  //     let fuel = calculateFuel(curr);
+  //     while (fuel) {
+  //       totalFuel += fuel;
+  //       fuel = calculateFuel(fuel);
+  //     }
+  //     return acc + totalFuel;
+  //   }, 0) // 5161601
+  // );
+  console.log(masses.reduce((acc, curr) => acc + calculateTotalFuel(curr), 0)); // 5161601
 })();
