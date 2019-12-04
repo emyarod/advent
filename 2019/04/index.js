ed45539beeaf5a85d176494dc4952fed873b18ef
emyarod <afw5059@gmail.com>
2019-12-04 00:36:27 -0800

    add 2019/04 part 2

--- 2019/04/index.js
+++ 2019/04/index.js
@@ -15,5 +15,22 @@ const countValidPasswords = ([start, end]) => {
   }
   return count;
 };
+const countValidPasswords2 = ([start, end]) => {
+  let count = 0;
+  for (let i = start; i <= end; i++) {
+    if (
+      `${i}`.match(/(\d)\1/u) &&
+      `${i}` === [...`${i}`].sort().join('') &&
+      `${i}`
+        .replace(/((\d)\2*)/gu, '$1,')
+        .split(',')
+        .some(str => str.length === 2)
+    ) {
+      count++;
+    }
+  }
+  return count;
+};
 
 console.log(countValidPasswords(input)); // 1650
+console.log(countValidPasswords2(input)); // 1129
